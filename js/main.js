fc35c3a3590cd93c4fac94fdc97ef0c848bef133
Aliaksandr <djvalentinoff@gmail.com>
2022-07-20 14:47:01 +0300

    fix: fixed bug index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 gsap.from(".hero__title", {opacity: 0, duration: 2, ease: "circle"});
 gsap.from(".hero__title", {y: 50, duration: 1.5, ease: "expo"});
-gsap.from(".hero__descr", {opacity: 0, delay: 0.5, duration: 2.5, ease: "circle"});
+gsap.from(".hero__descr", {opacity: 0, delay: 0.7, duration: 2.5, ease: "circle"});
 gsap.from(".hero__btn", {opacity: 0, duration: 2, ease: "circle"});
 gsap.from(".hero__btn", {y: 30, duration: 1.5, ease: "expo"});
 gsap.from("#photo-1", {opacity: 0, scale: 0.9, delay: 0.8, duration: 0.5, ease: "circle"});
 gsap.from("#photo-2", {opacity: 0, scale: 0.9, delay: 1, duration: 0.5, ease: "circle"});
 gsap.from("#photo-3", {opacity: 0, scale: 0.9, delay: 1.2, duration: 0.5, ease: "circle"});
-gsap.from(".photos__author", {opacity: 0, delay: 2, duration: 3, ease: "circle"});
+gsap.from(".photos__author", {opacity: 0, delay: 1.2, duration: 3, ease: "circle"});
 
 let tl = gsap.timeline({paused: true})
 tl.fromTo(".menu", {visibility: "hidden", y: 100, opacity: 0},
